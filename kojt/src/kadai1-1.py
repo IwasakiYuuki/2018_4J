f56155b606556ae5258cba19baa83b0ee14de3dc
yuuki <2445yuuki@gmail.com>
2018-12-26 15:28:48 +0900

    fuck

--- kojt/src/kadai1-1.py
+++ kojt/src/kadai1-1.py
@@ -45,20 +45,20 @@ def LPF(r, LPFactor):
 
 
 if __name__ == '__main__':
-    c = create_cos(1200, 1, 2000, -math.pi/2)
-    b = create_bpsk(6, 200)
-    vask = create_ask(c, b)
-    r = create_ask(vask, create_cos(1200, 1, 2000, -math.pi/2))
-    r = LPF(r, 0.9)
+    c = create_sin(1000, 1, 200, 0)
+#    b = create_bpsk(6, 200)
+#    vask = create_ask(c, b)
+#    r = create_ask(vask, create_cos(1200, 1, 2000, -math.pi/2))
+#    r = LPF(r, 0.9)
     with open('../txt/kadai1-1.txt', 'w') as f:
         for a in c:
             f.write(str(a[0]) + '  ' + str(a[1])+'\n')
-    with open('../txt/kadai1-2.txt', 'w') as f:
-        for a in b:
-            f.write(str(a[0]) + '  ' + str(a[1])+'\n')
-    with open('../txt/kadai1-3.txt', 'w') as f:
-        for data in vask:
-            f.write(str(data[0]) + '  ' + str(data[1])+'\n')
-    with open('../txt/kadai1-4_0.9.txt', 'w') as f:
-        for data in r:
-            f.write(str(data[0]) + '  ' + str(data[1])+'\n')
+#    with open('../txt/kadai1-2.txt', 'w') as f:
+#        for a in b:
+#            f.write(str(a[0]) + '  ' + str(a[1])+'\n')
+#    with open('../txt/kadai1-3.txt', 'w') as f:
+#        for data in vask:
+#            f.write(str(data[0]) + '  ' + str(data[1])+'\n')
+#    with open('../txt/kadai1-4_0.9.txt', 'w') as f:
+#        for data in r:
+#            f.write(str(data[0]) + '  ' + str(data[1])+'\n')
